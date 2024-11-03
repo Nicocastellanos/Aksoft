62dafcdfae1fe06e28da5957baa94b625d6de572
Nicolas Castellanos <nicolas.castellanos012@gmail.com>
2024-11-03 12:27:14 -0500

    first commit

---
+++ Ak_portafolio/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Titulo from './Titulo'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+
+        <Routes>
+          
+        </Routes>
+
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+
+export default App
